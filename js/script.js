7f113c075232432359cc725292953b2fbc2b1562
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-26 14:54:07 +0100

    Working on js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-// var
-var toDoList = [];
+$(document).ready(function() {
 
-// array with things to do
-toDoList = ['Buy tickets for the cinema', 'Buy some milk', 'Bake a cake', 'Go for a walk', 'Have a nice rest', 'Wake up at 07:00', 'Read a book'];
-console.log(toDoList);
+  // var
+  var toDoList = [];
+
+  // array with things to do
+  toDoList = ['Buy tickets for the cinema', 'Buy some milk', 'Bake a cake', 'Go for a walk', 'Have a nice rest', 'Wake up at 07:00', 'Read a book'];
+  console.log(toDoList);
+
+  // add array
+  
+
+  // check
+  $('.check').click(function() {
+    check($(this));
+  });
+
+  // cross
+  $('.cross').click(function() {
+    cross($(this));
+  });
+
+});
 
 
 // function
@@ -12,4 +29,16 @@ function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// check an item
+function check(select) {
+  var listItem = select.closest('li');
+  listItem.toggleClass('line-through');
+}
+
+// cross (remove) an item
+function cross(select) {
+  var listItem = select.closest('li');
+  listItem.remove();
 }
