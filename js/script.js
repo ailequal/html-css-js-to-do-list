545b5559d3534647f564f66a47c549efd9768070
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-26 15:39:17 +0100

    Working on adding extra to-do

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
 
   // var
+  var i = 0;
   var toDoList = [];
+  var listItem = '';
 
   // array with things to do
   toDoList = ['Buy tickets for the cinema', 'Buy some milk', 'Bake a cake', 'Go for a walk', 'Have a nice rest', 'Wake up at 07:00', 'Read a book'];
-  console.log(toDoList);
 
-  // add array
-  
+  // add array at to-do-list
+  for (i = 0; i < toDoList.length; i++) {
+    listItem = $('.template li').clone();
+    listItem.append(toDoList[i]);
+    $('.to-do-list').append(listItem);
+  }
+
+  // add
+  $('.add').click(function() {
+    add($(this));
+  });
 
   // check
   $('.check').click(function() {
@@ -29,6 +39,12 @@ function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// add an item
+function add(select) {
+  // var listItem = select.closest('li');
+  // listItem.toggleClass('line-through');
 }
 
 // check an item
